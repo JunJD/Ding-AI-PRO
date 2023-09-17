7dba6dd9ce9d0e2402ada30a484d342513d4525a
dingjunjie <864546065@qq.com>
2023-09-17 23:16:28 +0800

    chore: tailwindcss init

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,31 @@ module.exports = {
   content: [
     "./app/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
-    "./content/**/*.{md,mdx}",
   ],
-  darkMode: ["class"],
+  darkMode: ["class", ".dark-theme"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: ["var(--font-display)", "system-ui", "sans-serif"],
+        default: ["var(--font-default)", "system-ui", "sans-serif"],
+      },
+      colors: {
+        white: "var(--dingai-white)",
+        stone: {
+          50: "var(--dingai-stone-50)",
+          100: "var(--dingai-stone-100)",
+          200: "var(--dingai-stone-200)",
+          300: "var(--dingai-stone-300)",
+          400: "var(--dingai-stone-400)",
+          500: "var(--dingai-stone-500)",
+          600: "var(--dingai-stone-600)",
+          700: "var(--dingai-stone-700)",
+          800: "var(--dingai-stone-800)",
+          900: "var(--dingai-stone-900)",
+        },
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 }
+
